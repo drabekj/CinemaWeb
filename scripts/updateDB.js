37d955b4e475909772efa7ba0434f211067d8fa9
Drabek Jan <j.drabekj@gmail.com>
2016-11-11 23:30:40 +0800

    * Deleting/adding comments.

--- scripts/updateDB.js
+++ scripts/updateDB.js
@@ -7,33 +7,15 @@ function saveSeats(session){
         }
     }
 
+    // send request to update DB
     var screening_id = session.screening_id;
     var seat_array = session.seat_array;
-    console.log(screening_id);
-    console.log(seat_array);
     $.ajax({
            type: "post",
            url: "saveSeatsDB.php",
            data:{screening_id:screening_id,seat_array:seat_array},
            success: function(data){
-                alert("Seats sold!");
+                alert("Seats bought!");
            }
     });
-    // $.ajax({
-    //     url:'saveSeatsDB.php',
-    //     type:'post',
-    //     data:{session:session},
-    //     success: function(response){
-    //         $("#products li").remove();
-    //         $("#products").append(response);
-    //
-    //         if (columnName != "category") {
-    //             if(sort == "asc"){
-    //                 $("#sort").val("desc");
-    //             }else{
-    //                 $("#sort").val("asc");
-    //             }
-    //         }
-    //     }
-    // });
 }
