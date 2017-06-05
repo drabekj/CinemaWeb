43713938b2eb1716af473d6a630eb0855906aed6
Drabek Jan <drabek.honza@gmail.com>
2017-06-05 18:42:35 +0200

    * Fix setup script, symlinking /var/www/ with /vagrant/
        Results in creating /var/www/ dir and putting there source files.

--- .provision/setup.sh
+++ .provision/setup.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
 echo "Provisioning virtual machine..."
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get update > /dev/null
+    sudo apt-get -y upgrade > /dev/null
 
 # Installing GIT
 echo "Installing Git"
-    apt-get install git -y > /dev/null
+    sudo apt-get install git -y > /dev/null
 
 # Installing Nginx
 echo "Installing Nginx"
-    apt-get install nginx -y > /dev/null
+    sudo apt-get install nginx -y > /dev/null
 
 # Installing PHP
-echo "Updating PHP repository"
-    apt-get install python-software-properties build-essential -y > /dev/null
-    add-apt-repository ppa:ondrej/php5 -y > /dev/null
-    apt-get update > /dev/null
-
 echo "Installing PHP"
-    apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
+    sudo apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
 
 echo "Installing PHP extensions"
-    apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
+    sudo apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
 
 # Installing MySQL
-apt-get install debconf-utils -y > /dev/null
+sudo apt-get install debconf-utils -y > /dev/null
 debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
 
-apt-get install mysql-server -y > /dev/null
+sudo apt-get install mysql-server -y > /dev/null
 
 # Configure Nginx
 echo "Configuring Nginx"
-cp /var/www/.provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
+ # clean /var/www
+sudo rm -Rf /var/www
+
+ # symlink /var/www => /vagrant
+ln -s /vagrant /var/www
+
+sudo cp /var/www/.provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
 
